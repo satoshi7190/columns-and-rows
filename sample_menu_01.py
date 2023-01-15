039d9bcc969f748b1cfa9170f5f755e5ed394ebe
satoshi7190 <satoshi7190@gmail.com>
2023-01-16 00:42:25 +0900

    fix

--- sample_menu_01.py
+++ sample_menu_01.py
@@ -37,18 +37,112 @@ class SampleMenu01(QDialog):
         self.ui.mMapLayerComboBox.setFilters(QgsMapLayerProxyModel.VectorLayer)
 
         # すでに選択されているベクターレイヤーを取得
-        Startlayer = self.ui.mMapLayerComboBox.currentLayer()
+        setlayer = self.ui.mMapLayerComboBox.currentLayer()
 
-        # tableにセット
-        self.ui.mFieldComboBox.setLayer(Startlayer)
+        # FieldComboBoxにセット
+        self.ui.mFieldComboBox.setLayer(setlayer)
 
-        # レイヤーが選択されるたびにtableをセット
-        self.ui.mMapLayerComboBox.layerChanged["QgsMapLayer*"].connect(
-            self.ui.mFieldComboBox.setLayer
-        )
+        # レイヤーが選択されるたびにtableビューをセット
+        self.ui.mMapLayerComboBox.layerChanged["QgsMapLayer*"].connect(self.set_table)
+
+        # tableが選択されるたびにtableビューをセット
+        self.ui.mFieldComboBox.fieldChanged.connect(self.set_table)
+        
+        self.ui.checkBox.stateChanged.connect(self.set_table)
+        self.ui.checkBox_2.stateChanged.connect(self.set_table)
+        self.ui.checkBox_3.stateChanged.connect(self.set_table)
+        self.ui.checkBox_4.stateChanged.connect(self.set_table)
+
 
         self.ui.pushButton_run.clicked.connect(self.get_and_show_input_text)
         self.ui.pushButton_cancel.clicked.connect(self.close)
+
+        # テーブルを作成して値を設定
+        self.set_table()
+
+    def set_table(self):
+
+        # 選択されたレイヤーを取得
+        layer = self.ui.mMapLayerComboBox.currentLayer()
+        
+        # 選択されたfieldを取得
+        self.ui.mFieldComboBox.setLayer(layer)
+
+        # 項目名にする列
+        field_name = self.ui.mFieldComboBox.currentField()
+
+        # データタイプの取得
+        value_type = self.ui.comboBox.currentText()
+
+        # 空白削除オプション 先頭列
+        space_removal_first = self.ui.checkBox.checkState() == QtCore.Qt.Checked
+
+        # Unicode正規化オプション 先頭列
+        unicode_first = self.ui.checkBox_2.checkState() == QtCore.Qt.Checked
+
+        # 空白削除オプション 先頭列以外
+        space_removal_other = self.ui.checkBox_3.checkState() == QtCore.Qt.Checked
+
+        # Unicode正規化オプション 先頭列以外
+        unicode_other = self.ui.checkBox_4.checkState() == QtCore.Qt.Checked
+
+        front_row_name = str(field_name)
+
+        # 横列作成
+        self.ui.tableWidget.setColumnCount(layer.featureCount())
+        self.ui.tableWidget.setRowCount(len(layer.attributeList()))
+
+        Header_Labels_list = [front_row_name]
+
+        for feature in layer.getFeatures():
+
+            new_field_name = str(feature[field_name])
+
+            Header_Labels_list.append(new_field_name)
+
+        self.ui.tableWidget.setHorizontalHeaderLabels(Header_Labels_list)
+
+        for Column, field in enumerate(layer.fields()):
+            fix = field.name()
+
+            if fix == field_name:
+                continue
+
+            if space_removal_first:
+                fix = fix.replace(" ", "").replace("　", "")
+
+            if unicode_first:
+                fix = unicodedata.normalize("NFKC", fix)
+
+            list = [fix]
+
+            for feature in layer.getFeatures():
+
+                value = feature[field.name()]
+
+                if space_removal_other:
+
+                    value = str(feature[field.name()]).replace(" ", "").replace("　", "")
+
+                if unicode_other:
+
+                    value = unicodedata.normalize("NFKC", value)
+
+                if value_type == "string（文字）":
+                    exp = value
+
+                if value_type == "integer（少数を含まない数字）":
+                    exp = QgsExpression(f"to_int( {value} )").evaluate()
+
+                if value_type == "real（少数を含む数字）":
+                    exp = QgsExpression(f"to_real( {value} )").evaluate()
+
+                list.append(exp)
+
+                for row, deta in enumerate(list):
+
+                    self.ui.tableWidget.setItem(Column, row, QTableWidgetItem(f"{deta}"))
+
 
     def get_and_show_input_text(self):
 
@@ -151,3 +245,5 @@ class SampleMenu01(QDialog):
 
         # メッセージ表示
         QMessageBox.information(None, "メッセージ", "行列を入れ替えたジオメトリなしレイヤーを作成しました。")
+
+
